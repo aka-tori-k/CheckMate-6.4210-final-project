1176376a02ebe429b34231c0a1f51f0db1e5bed5
root <gguillen@mit.edu>
2025-11-21 04:18:32 +0000

    added rgbd sensor, physical model, and point cloud capabilities

---
+++ src/simulation_setup/add_rgbd_sensor.py
@@ -0,0 +1,60 @@
+from pydrake.all import (RgbdSensor, DepthRenderCamera,ColorRenderCamera, 
+                         RenderCameraCore, CameraInfo, ClippingRange, DepthRange, StartMeshcat,
+                         PointCloud, PeriodicEventData,Parser, RigidTransform, RotationMatrix)
+import numpy as np
+
+def add_rgbd_sensor(builder, plant, parser, scene_graph, iiwa):
+    """
+    Adds an RGB-D sensor to the simulation.
+    """
+    # Add a simple model to the plant
+    wrist_frame = plant.GetFrameByName("iiwa_link_7", iiwa)
+    frame_id = plant.GetBodyFrameIdOrThrow(wrist_frame.body().index())
+
+    #adjust X_PB if move sensor
+    sensor = RgbdSensor(
+        parent_id=frame_id,
+        X_PB=RigidTransform([0, 0, 0.16]),
+        color_camera=ColorRenderCamera(
+            core=RenderCameraCore(
+                renderer_name="vtk",
+                intrinsics=CameraInfo(width=640, height=480, fov_y=np.pi/4),
+                clipping = ClippingRange(0.01, 10.0),
+                X_BS=RigidTransform([0, 0, 0]),
+            )
+        ),
+        depth_camera=DepthRenderCamera(
+            core=RenderCameraCore(
+                renderer_name="vtk",
+                intrinsics=CameraInfo(width=640, height=480, fov_y=np.pi/4),
+                clipping = ClippingRange(0.01, 10.0),
+                X_BS=RigidTransform([0, 0, 0]),
+            ),
+            depth_range = DepthRange(0.01, 10.0)
+        )
+    )
+    # Add sensor system
+    rgbd = builder.AddSystem(sensor)
+
+    # Connect SG → sensor
+    builder.Connect(
+        scene_graph.get_query_output_port(),
+        rgbd.query_object_input_port()
+    )
+
+
+    camera_model = parser.AddModels("src/models/rgbd_sensor.sdf")[0]
+
+    wrist_frame = plant.GetFrameByName("iiwa_link_7", iiwa)
+    camera_frame = plant.GetFrameByName("base", camera_model)   
+
+    #adjust p if move physicl camera
+    plant.WeldFrames(wrist_frame, camera_frame, 
+                     RigidTransform(R=RotationMatrix.Identity(),p=np.array([0, 0, 0.11]).reshape((3, 1)))
+                     )
+    
+    return plant, rgbd
+
+if __name__ == "__main__":
+    # add_rgbd_sensor(builder, plant, scene_graph)
+    print("RGB-D sensor creation test completed successfully.")
--- src/simulation_setup/initialize_simulation.py
+++ src/simulation_setup/initialize_simulation.py
@@ -1,10 +1,14 @@
 from pydrake.all import (
     DiagramBuilder, AddMultibodyPlantSceneGraph, MeshcatVisualizer,
-    StartMeshcat, Simulator, RigidTransform, Parser
+    StartMeshcat, Simulator, Parser, MakeRenderEngineVtk, RenderEngineVtkParams, 
+    MeshcatPointCloudVisualizer, DepthImageToPointCloud, 
+    BaseField, CameraInfo, PixelType, RigidTransform, RotationMatrix
 )
 from add_pieces_to_plant import add_pieces_to_plant
 from set_initial_piece_poses import set_initial_piece_poses
 from add_robot_and_gripper import add_robot_and_gripper
+from add_rgbd_sensor import add_rgbd_sensor
+import numpy as np
 
 def initialize_simulation():
     meshcat = StartMeshcat()
@@ -12,12 +16,38 @@ def initialize_simulation():
     plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.0)
     parser = Parser(plant)
 
+    scene_graph.AddRenderer(
+    "vtk",
+    MakeRenderEngineVtk(RenderEngineVtkParams())
+    )
+
     board = parser.AddModels("src/models/checkerboard_8_8_0_1/checkerboard.sdf")[0]
     plant, instances = add_pieces_to_plant(plant, parser)
     plant, iiwa, wsg = add_robot_and_gripper(plant, parser)
+    plant, rgbd = add_rgbd_sensor(builder, plant, parser, scene_graph, iiwa)
 
+    pc_gen = builder.AddSystem(
+        DepthImageToPointCloud(
+            camera_info=CameraInfo(width=640, height=480, fov_y=np.pi/4),
+            pixel_type=PixelType.kDepth32F, 
+            scale=1.0,
+            fields = BaseField.kXYZs | BaseField.kRGBs
+            )
+        )
+        
+    # Connect images → pc generator
+    builder.Connect(rgbd.GetOutputPort("depth_image_32f"), pc_gen.depth_image_input_port())
+    builder.Connect(rgbd.GetOutputPort("color_image"), pc_gen.color_image_input_port())
+    builder.Connect(rgbd.GetOutputPort("body_pose_in_world"), pc_gen.GetInputPort("camera_pose"))
+
+    # Meshcat point cloud visualizer
+    pc_vis = builder.AddSystem(
+    MeshcatPointCloudVisualizer(meshcat, "rgbd_cloud", publish_period=0.1)
+    )
+    builder.Connect(pc_gen.point_cloud_output_port(), pc_vis.cloud_input_port())
+        
     visualizer = MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat)
-
+    
     plant.Finalize()
 
     diagram = builder.Build()
@@ -26,6 +56,18 @@ def initialize_simulation():
     plant_context = plant.GetMyMutableContextFromRoot(diagram_context)
 
     plant = set_initial_piece_poses(plant, plant_context, instances)
+
+    robot_initial_pose = np.array([
+        1.5,    # joint 1
+        -1,     # joint 2
+        0.0,    # joint 3
+        -1.5,   # joint 4
+        0.0,    # joint 5
+        1.8,    # joint 6
+        1.5     # joint 7
+        ])
+
+    plant.SetPositions(plant_context, iiwa, robot_initial_pose)
 
     simulator.Initialize()
 
