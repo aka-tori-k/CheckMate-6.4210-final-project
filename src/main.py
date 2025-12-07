f20668326ca49c418f7080c2f536c85cc4d4ea35
Brios Olivares <olivaresbrios@gmail.com>
2025-12-07 00:28:42 +0000

    outline integration logic in main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,38 @@
+from simulation_setup.initialize_simulation import initialize_simulation
 from chess_interface import ChessInterface
 
-if __name__ == "__main__":
-    # Test 5 moves
+
+def main():
+    # 1) initialize simulation
+    (simulator, plant, plant_context, meshcat, scene_graph, 
+    diagram_context, meshcat, diagram, traj_source,
+    logger_state, logger_desired, logger_torque) = initialize_simulation()
+   
+   # 2) initialize chess interface and get best move
     chess_interface = ChessInterface()
-    for i in range(5):
-        meta = chess_interface.get_best_move()
-        print(meta)
-        
-        chess_interface.apply_move(meta["move"])  # update board
+    move, from_square, to_square, moving_type, is_capture, captured_type = chess_interface.get_best_move().values()
+    print(f"Best move: {move}, from {from_square} to {to_square}, moving {moving_type}, capture: {is_capture}, captured type: {captured_type}")
+
+    # 3) compute_grasp_pose()
+    
+    # 4) compute_path() from current pose to grasp pose
+
+    # 5) execute_path() to grasp pose
+
+    # 6) grasp_piece()
+
+    # 7) compute_path to target square pose
+
+    # 8) execute_path to target square pose
+
+    # 9) release_piece()
+
+    # 10) return to initial pose
+
+    # 11) update internal board state
+    chess_interface.apply_move(move)  # update board
     
     chess_interface.close_engine()
+
+if __name__ == "__main__":
+    main()
